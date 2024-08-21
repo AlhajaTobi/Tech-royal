9c5be8440aba5bab415a7cade5190dcd39b87559
Alhaja <adeboyetobi@yahoo.come>
2024-08-21 10:19:23 -0700

    solve average

---
+++ classtask/classwork.py
@@ -0,0 +1,14 @@
+"""pseudo code
+1)Enter the radius,which is 10.
+2)store radius as number
+3)Enter pi_value as 3.142
+4)store pi_value
+5)Perform calculation to get area (3.142*radius*radius)
+6)Store and display result as Area.
+"""
+
+radius = 10
+#enter the radius,store radius
+PI_VALUE = 3.142
+#perform calculation("pi_value *radius *radius")
+print("area",3.142* 10**2)
---
+++ example.py
@@ -0,0 +1,7 @@
+firstnumber = int(input("enter the first number"))
+secondnumber = int(input("enter the first second number"))
+thirdnumber = int(input("enter the third number"))
+addition = firstnumber + secondnumber + thirdnumber
+average = addition /3
+ 
+print(average)

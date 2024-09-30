2db49db7f49a75f9239d9e854f90b87b10df6d6a
Alhaja <adeboyetobi@yahoo.come>
2024-09-30 07:56:04 -0700

    <phasegatenew>

--- question4.py
+++ question4.py
@@ -1,10 +1,4 @@
 import random
-
-number1 = random.randint(0, 1000)
-number2= random.randint(0, 1000)
-guess= int("what is the sum of the guessnumber:  ")
-
-correct_guess= (number1 + number2== guess)
-
-print(correct_guess)
+while (1000 %100):
+  		print(random.randrange(%n1000)
 	
